b5fb14a04265454fcf74743b3cd87e571fdd0c6c
mshiyaf <mshiyaf@gmail.com>
2017-11-22 23:48:35 +0530

    final

--- registration/server.php
+++ registration/server.php
@@ -68,7 +68,7 @@
 			if (mysqli_num_rows($results) == 1) {
 				$_SESSION['username'] = $username;
 				$_SESSION['success'] = "You are now logged in";
-				header('location: index.php');
+				header('location: ../student/stprofile.html');
 			}else {
 				array_push($errors, "Wrong username/password combination");
 			}
