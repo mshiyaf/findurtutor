e7922aeaf525bc7a043c6e30284a6eded8049ff4
mshiyaf <mshiyaf@gmail.com>
2017-11-22 13:29:32 +0530

    jkg

--- registration/server2.php
+++ registration/server2.php
@@ -1,74 +1,79 @@
 <?php
-session_start();
+	session_start();
 
-// variable declaration
-$username = "";
-$email    = "";
-$errors = array();
-$_SESSION['success'] = "";
+	// variable declaration
+	$username = "";
+	$email    = "";
+	$phone = "";
+	$area = "";
+	$errors = array();
+	$_SESSION['success'] = "";
 
-// connect to database
-$db = mysqli_connect('localhost', 'root', 'mysql', 'registration');
+	// connect to database
+	$db = mysqli_connect('localhost', 'root', 'mysql', 'registration') or die(mysql_error());
 
-// REGISTER USER
-if (isset($_POST['reg_user'])) {
-	// receive all input values from the form
-	$username = mysqli_real_escape_string($db, $_POST['username']);
-	$email = mysqli_real_escape_string($db, $_POST['email']);
-	$password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
-	$password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+	// REGISTER USER
+	if (isset($_POST['reg_user'])) {
+		// receive all input values from the form
+		$username = mysqli_real_escape_string($db, $_POST['username']);
+		$email = mysqli_real_escape_string($db, $_POST['email']);
+		$password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+		$password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+		$phone = mysqli_real_escape_string($db, $_POST['phone']);
+		$area = mysqli_real_escape_string($db, $_POST['area']);
+		$qualification = mysqli_real_escape_string($db, $_POST['qualification']);
 
-	// form validation: ensure that the form is correctly filled
-	if (empty($username)) { array_push($errors, "Username is required"); }
-	if (empty($email)) { array_push($errors, "Email is required"); }
-	if (empty($password_1)) { array_push($errors, "Password is required"); }
+		// form validation: ensure that the form is correctly filled
+		if (empty($username)) { array_push($errors, "Username is required"); }
+		if (empty($email)) { array_push($errors, "Email is required"); }
+		if (empty($password_1)) { array_push($errors, "Password is required"); }
+		if (empty($phone)) { array_push($errors, "Phone Number is required"); }
+		if (empty($area)) { array_push($errors, "Area is required"); }
+		if (empty($qualification)) { array_push($errors, "Qualification is required");
 
-	if ($password_1 != $password_2) {
-		array_push($errors, "The two passwords do not match");
-	}
+		if ($password_1 != $password_2) {
+			array_push($errors, "The two passwords do not match");
+		}
 
-	// register user if there are no errors in the form
-	if (count($errors) == 0) {
-		$password = md5($password_1);//encrypt the password before saving in the database
-		$query = "INSERT INTO tutor (username, email, password)
-				  VALUES('$username', '$email', '$password')";
-		mysqli_query($db, $query);
-
-		$_SESSION['username'] = $username;
-		$_SESSION['success'] = "You are now logged in";
-		header('location: index.php');
-	}
-
-}
-
-// ...
-// ...
-
-// LOGIN USER
-if (isset($_POST['login_user'])) {
-	$username = mysqli_real_escape_string($db, $_POST['username']);
-	$password = mysqli_real_escape_string($db, $_POST['password']);
-
-	if (empty($username)) {
-		array_push($errors, "Username is required");
-	}
-	if (empty($password)) {
-		array_push($errors, "Password is required");
-	}
-
-	if (count($errors) == 0) {
-		$password = md5($password);
-		$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
-		$results = mysqli_query($db, $query);
-
-		if (mysqli_num_rows($results) == 1) {
+		// register user if there are no errors in the form
+		if (count($errors) == 0) {
+			$password = md5($password_1);//encrypt the password before saving in the database
+			$query = "INSERT INTO tutor (username, email, password, phone, area, qualification)
+					  VALUES('$username', '$email', '$password', '$phone', '$area', '$qualification')";
+			mysqli_query($db, $query);
 			$_SESSION['username'] = $username;
 			$_SESSION['success'] = "You are now logged in";
-			header('location: ../profile/home.php');
-		}else {
-			array_push($errors, "Wrong username/password combination");
+			header('location: index.php');
+		}
+
+	}
+
+	// ...
+
+	// LOGIN USER
+	if (isset($_POST['login_user'])) {
+		$username = mysqli_real_escape_string($db, $_POST['username']);
+		$password = mysqli_real_escape_string($db, $_POST['password']);
+
+		if (empty($username)) {
+			array_push($errors, "Username is required");
+		}
+		if (empty($password)) {
+			array_push($errors, "Password is required");
+		}
+
+		if (count($errors) == 0) {
+			$password = md5($password);
+			$query = "SELECT * FROM student WHERE username='$username' AND password='$password'";
+			$results = mysqli_query($db, $query);
+
+			if (mysqli_num_rows($results) == 1) {
+				$_SESSION['username'] = $username;
+				$_SESSION['success'] = "You are now logged in";
+				header('location: index.php');
+			}else {
+				array_push($errors, "Wrong username/password combination");
+			}
 		}
 	}
-}
-
 ?>
